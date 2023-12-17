7a1053cd550547e0fb40159a4c6d9065fb40c986
afg9327 <93969640+afg9327@users.noreply.github.com>
2023-12-17 17:27:13 +0900

    JetPackCompose CardDealer Completed

---
+++ app/src/main/java/com/example/week12carddealer/CardViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.week12carddealer
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import kotlin.random.Random
+
+
+class CardViewModel: ViewModel(){
+    private var _cards = MutableLiveData<IntArray>(IntArray(5){0})
+    val cards : LiveData<IntArray> = _cards
+    fun shuffle() {
+        var num = 0
+        val newCards = IntArray(5) {-1}
+        for (i in newCards.indices) {
+            // 중복 검사
+            do {
+                num = Random.nextInt(52)
+            } while (newCards.contains(num))
+            newCards[i] = num
+        }
+
+        // 정렬
+        newCards.sort()
+
+        _cards.value = newCards
+    }
+}
